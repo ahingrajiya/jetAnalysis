99fcf9b1add8af0be4bb0adfc9a0a8aea6c34596
ahingrajiya <abhi.h123@gmail.com>
2024-06-29 20:49:32 -0500

    changes made to accomodate jet collection name

--- Event.cc
+++ Event.cc
@@ -4,9 +4,9 @@
  * @brief Internal event structure of the framework
  * @version 0.1
  * @date 2023-10-19
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 // Jet analysis headers
@@ -20,11 +20,11 @@
 
 //________________
 Event::Event() : TObject(), fRunId{0}, fEventId{0}, fLumi{0},
-                 fVx{0}, fVy{0}, fVz{0}, fHiBin{-1}, fPtHat{-1}, fPtHatWeight{-1}, 
-                 fNBadPFJets{0},  fNBadCaloJets{0}, fMult{0},
-                 fGenJetsCollectionIsFilled{kFALSE} {
-    fPFJetCollection = new PartFlowJetCollection{};
-    fCaloJetCollection = new CaloJetCollection{};
+                 fVx{0}, fVy{0}, fVz{0}, fHiBin{-1}, fPtHat{-1}, fPtHatWeight{-1},
+                 fNBadJets{0}, fMult{0},
+                 fGenJetsCollectionIsFilled{kFALSE}
+{
+    fRecoJetCollection = new RecoJetCollection{};
     fGenJetCollection = new GenJetCollection{};
     fTrackCollection = new TrackCollection{};
     fGenTrackCollection = new GenTrackCollection{};
@@ -32,20 +32,19 @@ Event::Event() : TObject(), fRunId{0}, fEventId{0}, fLumi{0},
 }
 
 //________________
-Event::Event(const UInt_t& runId, const ULong64_t& eventId, const UInt_t& lumi, 
-             const Float_t& vx, const Float_t& vy, const Float_t& vz, 
-             const Int_t& hiBin, const Float_t& ptHat, 
-             const Float_t& w, const Int_t& nBadPFJets, 
-             const Int_t& nBadCaloJets, const Int_t& mult) : TObject(),
-    fRunId{runId}, fEventId{eventId}, fLumi{lumi}, 
-    fVx{vx}, fVy{vy}, fVz{vz},
-    fHiBin{(Short_t)hiBin}, fPtHat{ptHat}, fPtHatWeight{w}, 
-    fNBadPFJets{(UChar_t)nBadPFJets}, fNBadCaloJets{(UChar_t)nBadCaloJets},
-    fMult{(UShort_t)mult}, fGenJetsCollectionIsFilled{kFALSE} {
-    
-    // Create new collections 
-    fPFJetCollection = new PartFlowJetCollection{};
-    fCaloJetCollection = new CaloJetCollection{};
+Event::Event(const UInt_t &runId, const ULong64_t &eventId, const UInt_t &lumi,
+             const Float_t &vx, const Float_t &vy, const Float_t &vz,
+             const Int_t &hiBin, const Float_t &ptHat,
+             const Float_t &w, const Int_t &nBadJets, const Int_t &mult) : TObject(),
+                                                                           fRunId{runId}, fEventId{eventId}, fLumi{lumi},
+                                                                           fVx{vx}, fVy{vy}, fVz{vz},
+                                                                           fHiBin{(Short_t)hiBin}, fPtHat{ptHat}, fPtHatWeight{w},
+                                                                           fNBadJets{(UChar_t)nBadJets},
+                                                                           fMult{(UShort_t)mult}, fGenJetsCollectionIsFilled{kFALSE}
+{
+
+    // Create new collections
+    fRecoJetCollection = new RecoJetCollection{};
     fGenJetCollection = new GenJetCollection{};
     fTrackCollection = new TrackCollection{};
     fGenTrackCollection = new GenTrackCollection{};
@@ -53,38 +52,40 @@ Event::Event(const UInt_t& runId, const ULong64_t& eventId, const UInt_t& lumi,
 }
 
 //________________
-Event::~Event() {
+Event::~Event()
+{
     // Clean collection of particle jets
-    for (PartFlowJetIterator iter=fPFJetCollection->begin();
-         iter!=fPFJetCollection->end(); iter++) {
-        delete *iter;
-    }
-    // Clean collection of calorimeter jets
-    for (CaloJetIterator iter=fCaloJetCollection->begin();
-         iter!=fCaloJetCollection->end(); iter++) {
+    for (RecoJetIterator iter = fRecoJetCollection->begin();
+         iter != fRecoJetCollection->end(); iter++)
+    {
         delete *iter;
     }
     // Clean collection of generated jets
-    for (GenJetIterator iter=fGenJetCollection->begin();
-         iter!=fGenJetCollection->end(); iter++) {
+    for (GenJetIterator iter = fGenJetCollection->begin();
+         iter != fGenJetCollection->end(); iter++)
+    {
         delete *iter;
     }
     // Clean track collection
-    for (TrackIterator iter=fTrackCollection->begin();
-         iter!=fTrackCollection->end(); iter++) {
+    for (TrackIterator iter = fTrackCollection->begin();
+         iter != fTrackCollection->end(); iter++)
+    {
         delete *iter;
     }
     // Clean MC track collection
-    for (GenTrackIterator iter=fGenTrackCollection->begin();
-         iter!=fGenTrackCollection->end(); iter++) {
+    for (GenTrackIterator iter = fGenTrackCollection->begin();
+         iter != fGenTrackCollection->end(); iter++)
+    {
         delete *iter;
     }
     // Clear trigger and skim instance
-    if (fTrigAndSkim) delete fTrigAndSkim;
+    if (fTrigAndSkim)
+        delete fTrigAndSkim;
 }
 
 //________________
-void Event::print() {
+void Event::print()
+{
     std::cout << Form("-------------------------------------\n")
               << Form("runId: %d  eventId: %llu  lumi: %d  vx: %5.2f  vy: %5.2f  vz: %5.2f\n", fRunId, fEventId, fLumi, fVx, fVy, fVz)
               << Form("hiBin: %d  ptHat: %3.2f  ptHatWeight: %4.2f \n", hiBin(), fPtHat, fPtHatWeight)
--- Event.h
+++ Event.h
@@ -4,9 +4,9 @@
  * @brief Internal event structure of the framework
  * @version 0.1
  * @date 2023-10-19
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #ifndef Event_h
@@ -18,147 +18,139 @@
 #include "TriggerAndSkim.h"
 
 //________________
-class Event : public TObject {
-  public:
-    /// @brief Default constructor
-    Event();
-    /// @brief Parametrized constructor
-    Event(const UInt_t& runId, const ULong64_t& eventId, const UInt_t& lumi, 
-          const Float_t& vx, const Float_t& vy, const Float_t& vz, 
-          const Int_t& hiBin, const Float_t& ptHat, 
-          const Float_t& w, const Int_t& nBadPFJets, 
-          const Int_t& nBadCaloJets, const Int_t& mult);
-    /// @brief Destructor
-    virtual ~Event();
+class Event : public TObject
+{
+public:
+  /// @brief Default constructor
+  Event();
+  /// @brief Parametrized constructor
+  Event(const UInt_t &runId, const ULong64_t &eventId, const UInt_t &lumi,
+        const Float_t &vx, const Float_t &vy, const Float_t &vz,
+        const Int_t &hiBin, const Float_t &ptHat,
+        const Float_t &w, const Int_t &nBadJets, const Int_t &mult);
+  /// @brief Destructor
+  virtual ~Event();
 
-    //
-    // Setters
-    //
+  //
+  // Setters
+  //
 
-    /// @brief Set run index
-    void setRunId(const UInt_t& id)      { fRunId = id; }
-    /// @brief Set event index
-    void setEventId(const ULong64_t& id) { fEventId = id; }
-    /// @brief Set luminosity
-    void setLumi(const UInt_t& lumi)     { fLumi = lumi; }
-    /// @brief Set vertex x position
-    void setVx(const Float_t& vx)        { fVx = vx; }
-    /// @brief Set vertex y position
-    void setVy(const Float_t& vy)        { fVy = vy; }
-    /// @brief Set vertex z position
-    void setVz(const Float_t& vz)        { fVz = vz; }
-    /// @brief Set vertex x, y, and z coordinates
-    void setVertex(const Float_t& x, const Float_t& y, const Float_t &z) { fVx = x; fVy = y; fVz = z; }
-    /// @brief Set centrality bin (0-200)
-    void setHiBin(const Int_t& hiBin)    { fHiBin = hiBin; }
-    /// @brief Set ptHat
-    void setPtHat(const Float_t& ptHat)  { fPtHat = ptHat; }
-    /// @brief Set event weight
-    void setPtHatWeight(const Float_t& w) { fPtHatWeight = w; }
-    /// @brief Set number of particle flow jets with pT > pThat
-    void setNumberOfOverscaledPFJets(const Int_t& n) { fNBadPFJets = (UChar_t)n; }
-    /// @brief Set number of calorimeter jets with pT > pThat
-    void setNumberOfOverscaledCaloJets(const Int_t& n) { fNBadCaloJets = (UChar_t)n; }
-    /// @brief Set reference multiplicity (CMS way)
-    void setMultiplicity(const Int_t& mult) { fMult = (UShort_t)mult; }
-    /// @brief Set the flag that generated jet collection is filled to true
-    void setGenJetCollectionIsFilled()      { fGenJetsCollectionIsFilled = kTRUE; }
-    /// @brief  Print event information
-    void print();
+  /// @brief Set run index
+  void setRunId(const UInt_t &id) { fRunId = id; }
+  /// @brief Set event index
+  void setEventId(const ULong64_t &id) { fEventId = id; }
+  /// @brief Set luminosity
+  void setLumi(const UInt_t &lumi) { fLumi = lumi; }
+  /// @brief Set vertex x position
+  void setVx(const Float_t &vx) { fVx = vx; }
+  /// @brief Set vertex y position
+  void setVy(const Float_t &vy) { fVy = vy; }
+  /// @brief Set vertex z position
+  void setVz(const Float_t &vz) { fVz = vz; }
+  /// @brief Set vertex x, y, and z coordinates
+  void setVertex(const Float_t &x, const Float_t &y, const Float_t &z)
+  {
+    fVx = x;
+    fVy = y;
+    fVz = z;
+  }
+  /// @brief Set centrality bin (0-200)
+  void setHiBin(const Int_t &hiBin) { fHiBin = hiBin; }
+  /// @brief Set ptHat
+  void setPtHat(const Float_t &ptHat) { fPtHat = ptHat; }
+  /// @brief Set event weight
+  void setPtHatWeight(const Float_t &w) { fPtHatWeight = w; }
+  /// @brief Set number of particle flow jets with pT > pThat
+  void setNumberOfOverscaledPFJets(const Int_t &n) { fNBadJets = (UChar_t)n; }
+  /// @brief Set reference multiplicity (CMS way)
+  void setMultiplicity(const Int_t &mult) { fMult = (UShort_t)mult; }
+  /// @brief Set the flag that generated jet collection is filled to true
+  void setGenJetCollectionIsFilled() { fGenJetsCollectionIsFilled = kTRUE; }
+  /// @brief  Print event information
+  void print();
 
-    //
-    // Getters
-    //
+  //
+  // Getters
+  //
 
-    /// @brief Return run index 
-    UInt_t runId() const      { return fRunId; }
-    /// @brief Return event index 
-    ULong64_t eventId() const { return fEventId; }
-    /// @brief Return luminosity 
-    UInt_t lumi() const       { return fLumi; }
-    /// @brief Return vertex z 
-    Float_t vz() const        { return fVz; }
-    /// @brief Return hiBin bin 
-    Int_t hiBin() const       { return (Int_t)fHiBin; }
-    /// @brief Return centrality bin
-    Double_t centrality() const  { return (fHiBin < 0) ? -5 : 100. - Double_t(200 - fHiBin) * 0.5; }
-    /// @brief Return ptHat 
-    Float_t ptHat() const     { return fPtHat; }
-    /// @brief Return event weight 
-    Float_t ptHatWeight() const    { return fPtHatWeight; }
-    /// @brief Return number of particle flow jets in even with pT > pThat 
-    Int_t numberOfOverscaledPFJets() const { return (Int_t)fNBadPFJets; }
-    /// @brief Return number of calorimeter jets in even with pT > pThat 
-    Int_t numberOfOverscaledCaloJets() const { return (Int_t)fNBadCaloJets; }
-    /// @brief Return reference multiplicity (CMS way) 
-    Int_t multiplicity() const { return (Int_t)fMult; }
-    /// @brief Return if generated jet collection filled 
-    Bool_t isGenJetCollectionFilled() const { return fGenJetsCollectionIsFilled; }
-    /// @brief Return number of particle flow jets 
-    UInt_t numberOfPFJets() const   { return this->pfJetCollection()->size(); }
-    /// @brief Return number of calorimeter jets 
-    UInt_t numberOfCaloJets() const { return this->caloJetCollection()->size(); }
-    /// @brief Return number of generated jets 
-    UInt_t numberOfGenJets() const  { return this->genJetCollection()->size(); }
+  /// @brief Return run index
+  UInt_t runId() const { return fRunId; }
+  /// @brief Return event index
+  ULong64_t eventId() const { return fEventId; }
+  /// @brief Return luminosity
+  UInt_t lumi() const { return fLumi; }
+  /// @brief Return vertex z
+  Float_t vz() const { return fVz; }
+  /// @brief Return hiBin bin
+  Int_t hiBin() const { return (Int_t)fHiBin; }
+  /// @brief Return centrality bin
+  Double_t centrality() const { return (fHiBin < 0) ? -5 : 100. - Double_t(200 - fHiBin) * 0.5; }
+  /// @brief Return ptHat
+  Float_t ptHat() const { return fPtHat; }
+  /// @brief Return event weight
+  Float_t ptHatWeight() const { return fPtHatWeight; }
+  /// @brief Return number of particle flow jets in even with pT > pThat
+  Int_t numberOfOverscaledPFJets() const { return (Int_t)fNBadJets; }
+  /// @brief Return reference multiplicity (CMS way)
+  Int_t multiplicity() const { return (Int_t)fMult; }
+  /// @brief Return if generated jet collection filled
+  Bool_t isGenJetCollectionFilled() const { return fGenJetsCollectionIsFilled; }
+  /// @brief Return number of particle flow jets
+  UInt_t numberOfRecoJets() const { return this->recoJetCollection()->size(); }
+  /// @brief Return number of generated jets
+  UInt_t numberOfGenJets() const { return this->genJetCollection()->size(); }
 
-    /// @brief Return pointer to a collection of tracks
-    TrackCollection *trackCollection() const { return fTrackCollection; }
-    /// @brief Return pointer to a collection of MC tracks 
-    GenTrackCollection *genTrackCollection() const { return fGenTrackCollection; }
-    /// @brief Return pointer to a collection of particle flow jets 
-    PartFlowJetCollection *pfJetCollection() const { return fPFJetCollection; }
-    /// @brief Return pointer to a collection of calorimeter jets 
-    CaloJetCollection *caloJetCollection() const { return fCaloJetCollection; }
-    /// @brief Return pointer to a collection of generated jets 
-    GenJetCollection *genJetCollection() const { return fGenJetCollection; }
-    /// @brief Return pointer to a trigger and skimming information 
-    TriggerAndSkim *trigAndSkim() const { return fTrigAndSkim; }
+  /// @brief Return pointer to a collection of tracks
+  TrackCollection *trackCollection() const { return fTrackCollection; }
+  /// @brief Return pointer to a collection of MC tracks
+  GenTrackCollection *genTrackCollection() const { return fGenTrackCollection; }
+  /// @brief Return pointer to a collection of reco jets
+  RecoJetCollection *recoJetCollection() const { return fRecoJetCollection; }
+  /// @brief Return pointer to a collection of generated jets
+  GenJetCollection *genJetCollection() const { return fGenJetCollection; }
+  /// @brief Return pointer to a trigger and skimming information
+  TriggerAndSkim *trigAndSkim() const { return fTrigAndSkim; }
 
-  private:
-    /// @brief Run index
-    UInt_t    fRunId;
-    /// @brief Event index
-    ULong64_t fEventId;
-    /// @brief Luminosity value
-    UInt_t    fLumi;
-    /// @brief Vertex x position
-    Float_t   fVx;
-    /// @brief Vertex y position
-    Float_t   fVy;
-    /// @brief Vertex z position
-    Float_t   fVz;
-    /// @brief Centrality bin
-    Short_t   fHiBin;
-    /// @brief pthat sclaing
-    Float_t   fPtHat;
-    /// @brief Event weight scaling
-    Float_t   fPtHatWeight;
-    /// @brief Number of particle flow jets with pT > pThat
-    UChar_t   fNBadPFJets;
-    /// @brief Number of calorimeter jets with pT > pThat
-    UChar_t   fNBadCaloJets;
-    /// @brief Reference charged track multiplicity (CMS way)
-    UShort_t  fMult;
-    /// @brief Check if collection of generated jets is filled
-    Bool_t    fGenJetsCollectionIsFilled;
+private:
+  /// @brief Run index
+  UInt_t fRunId;
+  /// @brief Event index
+  ULong64_t fEventId;
+  /// @brief Luminosity value
+  UInt_t fLumi;
+  /// @brief Vertex x position
+  Float_t fVx;
+  /// @brief Vertex y position
+  Float_t fVy;
+  /// @brief Vertex z position
+  Float_t fVz;
+  /// @brief Centrality bin
+  Short_t fHiBin;
+  /// @brief pthat sclaing
+  Float_t fPtHat;
+  /// @brief Event weight scaling
+  Float_t fPtHatWeight;
+  /// @brief Number of jets with pT > pThat
+  UChar_t fNBadJets;
+  /// @brief Reference charged track multiplicity (CMS way)
+  UShort_t fMult;
+  /// @brief Check if collection of generated jets is filled
+  Bool_t fGenJetsCollectionIsFilled;
 
-    /// @brief Particle flow jet collection
-    PartFlowJetCollection *fPFJetCollection;
-    /// @brief Calorimeter jet collection
-    CaloJetCollection *fCaloJetCollection;
-    /// @brief Generated jet collection
-    GenJetCollection *fGenJetCollection;
+  /// @brief Reco jet collection
+  RecoJetCollection *fRecoJetCollection;
+  /// @brief Generated jet collection
+  GenJetCollection *fGenJetCollection;
 
-    /// @brief Track collection
-    TrackCollection *fTrackCollection;
-    /// @brief MC track collection
-    GenTrackCollection *fGenTrackCollection;
+  /// @brief Track collection
+  TrackCollection *fTrackCollection;
+  /// @brief MC track collection
+  GenTrackCollection *fGenTrackCollection;
 
+  /// @brief Trigger and skimming information
+  TriggerAndSkim *fTrigAndSkim;
 
-    /// @brief Trigger and skimming information
-    TriggerAndSkim *fTrigAndSkim;
-
-    ClassDef(Event, 1)
+  ClassDef(Event, 1)
 };
 
 #endif // #define Event_h
