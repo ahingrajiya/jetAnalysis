22a092373039221fddc7156c4d345ee6acf3065f
ahingrajiya <abhi.h123@gmail.com>
2024-06-29 20:10:47 -0500

    added gen tracks reader

--- GenTrack.cc
+++ GenTrack.cc
@@ -4,22 +4,24 @@
  * @brief Generated track description
  * @version 0.1
  * @date 2023-10-19
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 // Jet analysis headers
 #include "GenTrack.h"
-
+#include "TString.h"
+#include <iostream>
 ClassImp(GenTrack)
 
-//___________________
-GenTrack::GenTrack() : TObject() {
-
+    //___________________
+    GenTrack::GenTrack() : BaseTrack{}, fPDGID{0}, fSube{0}
+{
 }
 
-//___________________
-GenTrack::~GenTrack() {
-
+void GenTrack::print()
+{
+    std::cout << "-- Generated Tracks Info --\n"
+              << Form("TrkPt: %5.2f TrkEta: %3.2f TrkPhi: %3.2f PDGID: %i Sube: %c", this->TrkPt(), this->TrkEta(), this->TrkPhi(), this->TrkPDGID(), this->TrkSube());
 }
--- GenTrack.h
+++ GenTrack.h
@@ -4,9 +4,9 @@
  * @brief Generated track description
  * @version 0.1
  * @date 2023-10-19
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #ifndef GenTrack_h
@@ -14,16 +14,40 @@
 
 // ROOT headers
 #include "TObject.h"
+#include "BaseTrack.h"
 
 //_________________
-class GenTrack : public TObject {
-  public:
-    GenTrack();
-    virtual ~GenTrack();
+class GenTrack : public BaseTrack
+{
+public:
+  /// @brief constructor
+  GenTrack();
+  /// @brief destructor
+  virtual ~GenTrack() {}
 
-  private:
+  /// Setter
 
-    ClassDef(GenTrack, 1)
+  ///@brief Set Particle Identification
+  void setPDGID(const Int_t &pdg) { fPDGID = {pdg}; }
+  ///@brief Set Subevent
+  void setSube(const Char_t &sube) { fSube = {sube}; }
+  ///@brief Print parameters of tracks
+  void print();
+
+  // Getter
+
+  ///@brief Get PDGID
+  Int_t TrkPDGID() const { return fPDGID; }
+  ///@brief Get Sube
+  Char_t TrkSube() const { return fSube; }
+
+private:
+  ///@brief Particle ID
+  Int_t fPDGID;
+  ///@brief Subevent Identification
+  Char_t fSube;
+
+  ClassDef(GenTrack, 1)
 };
 
 #endif // #define GenTrack_h
